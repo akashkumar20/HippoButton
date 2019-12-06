45b5c93c3a81e340839d1a2e59e9fcb072d3d18c
akashkumar20 <akash@iofactory.in>
2019-12-06 15:52:50 +0530

    removed comment

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
 import React from 'react';
 import './App.css';
 
-// import Test from './test'
 import Hippo from './hippo/hippo';
-
-
-// import { createStore } from 'redux';
-// import todoApp from './reducer'
-
-// import {
-//     addTodo,
-//     toggleTodo,
-//     setVisibilityFilter,
-//     VisibilityFilters
-//   } from './actions'
 
 class App extends React.Component{
   
   render(){
-    // const store = createStore(todoApp)
-
-    // // console.log(store.getState())
-
-    // const unsubscribe = store.subscribe(()=> console.log(store.getState()))
-
-    // store.dispatch(addTodo('Learn about actions'))
-    // store.dispatch(addTodo('Learn about reducers'))
-    // store.dispatch(addTodo('Learn about store'))
-    // store.dispatch(toggleTodo(0))
-    // store.dispatch(toggleTodo(1))
-    // store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
-
-    // unsubscribe()
-
-    // console.log(store.getState().todos[0].text, store.getState().todos[0].completed)
     return(<Hippo/>);
   }
 }
